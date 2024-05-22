cd988dcbb04a1fd05955c70f8fc0dcab5a4b329c
onedhonyreff <onedhonyreff@gmail.com>
2024-05-22 10:38:54 +0700

    initial repo

--- api/index.js
+++ api/index.js
@@ -1,52 +1,52 @@
-// import { Client } from "whatsapp-web.js";
+import { Client } from "whatsapp-web.js";
 import express from "express";
 import bodyParser from "body-parser";
 // import qrcode from "qrcode-terminal";
 
 let currentQrText = "QR not available";
 
-// const client = new Client({
-//     puppeteer: {
-//         headless: true,
-//         args: ["--no-sandbox", "--disable-gpu"],
-//     },
-//     webVersionCache: {
-//         type: "remote",
-//         remotePath:
-//             "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
-//     },
-// });
+const client = new Client({
+    puppeteer: {
+        headless: true,
+        args: ["--no-sandbox", "--disable-gpu"],
+    },
+    webVersionCache: {
+        type: "remote",
+        remotePath:
+            "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+    },
+});
 
-// client.on("qr", (qr) => {
-//     console.log("QR RECEIVED", qr);
-//     if (qr) {
-//         qrcode.generate(qr, { small: true });
-//         currentQrText = qr;
-//     }
-// });
+client.on("qr", (qr) => {
+    console.log("QR RECEIVED", qr);
+    if (qr) {
+        // qrcode.generate(qr, { small: true });
+        currentQrText = qr;
+    }
+});
 
-// client.on("ready", () => {
-//     console.log("Client is ready!");
-// });
+client.on("ready", () => {
+    console.log("Client is ready!");
+});
 
-// client.on("message", (msg) => {
-//     console.log("pesan masuk");
-//     console.log(msg);
-//     msg.getChat().then((chat) => {
-//         chat.sendStateTyping();
-//     });
-//     if (msg.body == "!ping") {
-//         msg.reply("pong");
-//     }
-// });
+client.on("message", (msg) => {
+    console.log("pesan masuk");
+    console.log(msg);
+    msg.getChat().then((chat) => {
+        chat.sendStateTyping();
+    });
+    if (msg.body == "!ping") {
+        msg.reply("pong");
+    }
+});
 
-// client.on("disconnected", (event) => {
-//     if (event === "NAVIGATION") {
-//         client.initialize();
-//     }
-// });
+client.on("disconnected", (event) => {
+    if (event === "NAVIGATION") {
+        client.initialize();
+    }
+});
 
-// client.initialize();
+client.initialize();
 
 function handleDefault(req, res) {
     res.status(200).json({
