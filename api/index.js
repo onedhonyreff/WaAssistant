6d30429079575e09e5781ed4dda75c2ccd91d56b
onedhonyreff <onedhonyreff@gmail.com>
2024-05-22 11:57:31 +0700

    initial repo

--- api/index.js
+++ api/index.js
@@ -5,48 +5,52 @@ import bodyParser from "body-parser";
 
 let currentQrText = "QR not available";
 
-const client = new Client({
-    puppeteer: {
-        headless: true,
-        args: ["--no-sandbox", "--disable-gpu"],
-    },
-    webVersionCache: {
-        type: "remote",
-        remotePath:
-            "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
-    },
-});
-
-client.on("qr", (qr) => {
-    console.log("QR RECEIVED", qr);
-    if (qr) {
-        // qrcode.generate(qr, { small: true });
-        currentQrText = qr;
-    }
-});
-
-client.on("ready", () => {
-    console.log("Client is ready!");
-});
-
-client.on("message", (msg) => {
-    console.log("pesan masuk");
-    console.log(msg);
-    msg.getChat().then((chat) => {
-        chat.sendStateTyping();
+const launchWhatsAppClient = async () => {
+    const client = new Client({
+        puppeteer: {
+            headless: true,
+            args: ["--no-sandbox", "--disable-gpu"],
+        },
+        webVersionCache: {
+            type: "remote",
+            remotePath:
+                "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+        },
     });
-    if (msg.body == "!ping") {
-        msg.reply("pong");
-    }
-});
 
-client.on("disconnected", (event) => {
-    if (event === "NAVIGATION") {
-        client.initialize();
-    }
-});
+    client.on("qr", (qr) => {
+        console.log("QR RECEIVED", qr);
+        if (qr) {
+            // qrcode.generate(qr, { small: true });
+            currentQrText = qr;
+        }
+    });
 
-client.initialize();
+    client.on("ready", () => {
+        console.log("Client is ready!");
+    });
+
+    client.on("message", (msg) => {
+        console.log("pesan masuk");
+        console.log(msg);
+        msg.getChat().then((chat) => {
+            chat.sendStateTyping();
+        });
+        if (msg.body == "!ping") {
+            msg.reply("pong");
+        }
+    });
+
+    client.on("disconnected", (event) => {
+        if (event === "NAVIGATION") {
+            client.initialize();
+        }
+    });
+
+    client.initialize();
+};
+
+launchWhatsAppClient();
 
 function handleDefault(req, res) {
     res.status(200).json({
